422b4a5e614edac05dcb380a645026aef0542dce
Sergey Rubtsov <dart.moal@gmail.com>
2012-12-07 14:04:25 +0900

    deleted CommandForm, added search.png

--- src/main/java/com/roles/assignment/ui/person/PersonWidget.java
+++ src/main/java/com/roles/assignment/ui/person/PersonWidget.java
@@ -9,8 +9,6 @@ import com.roles.assignment.ui.EntityForm;
 import com.roles.assignment.ui.help.HelpWindow;
 import com.roles.assignment.ui.persons.PersonReferenceContainer;
 import com.roles.assignment.ui.persons.SearchFilter;
-import com.roles.assignment.ui.persons.SearchView;
-import com.roles.assignment.ui.persons.SharingOptions;
 import com.vaadin.data.Property;
 import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.event.ItemClickEvent;
@@ -26,17 +24,15 @@ import org.springframework.beans.factory.annotation.Configurable;
 public class PersonWidget extends VerticalLayout implements
         Button.ClickListener, Property.ValueChangeListener, ItemClickListener, Widget {
     private Button newPerson = new Button("Add person");
+    private Button newUser = new Button("Add user");
     private Button search = new Button("Search");
-    private Button share = new Button("Share");
     private Button help = new Button("Help");
-    private Button main = new Button("Main");
     private Button save = new Button("Save");
     private Button discard = new Button("Discard");
     private HorizontalSplitPanel horizontalSplit = new HorizontalSplitPanel();
 
     private LeftForm leftForm;
     private PersonList personList = null;
-    private CommandForm commandForm = null;
     private PersonForm personForm = null;
     private UserForm userForm = null;
     private HelpWindow helpWindow = null;
@@ -65,7 +61,6 @@ public class PersonWidget extends VerticalLayout implements
         this.setExpandRatio(horizontalSplit, 1);
         horizontalSplit.setSplitPosition(270, HorizontalSplitPanel.UNITS_PIXELS);
         horizontalSplit.setFirstComponent(setLeftForm(FormType.COMMANDS));
-        main.addListener((Button.ClickListener) this);
         save.addListener((Button.ClickListener) this);
         discard.addListener((Button.ClickListener) this);
 
@@ -117,25 +112,17 @@ public class PersonWidget extends VerticalLayout implements
     }
 
     public LeftForm setLeftForm(FormType type) {
-        if (type == FormType.COMMANDS) {
-            getLeftForm().removeAllComponents();
-            leftForm.addComponent(getCommandForm());
-            return leftForm;
-        } else
         if (type == FormType.PERSON) {
             getLeftForm().setForm(getPersonForm());
-            leftForm.addComponent(main);
             return leftForm;
         } else
         if (type == FormType.USER) {
             getLeftForm().setForm(getUserForm());
-            leftForm.addComponent(main);
             return leftForm;
         } else
         if (type == FormType.FIND) {
             getLeftForm().removeAllComponents();
             leftForm.addComponent(new Label(type.name()));
-            leftForm.addComponent(main);
             return leftForm;
         }
         return getLeftForm();
@@ -154,13 +141,6 @@ public class PersonWidget extends VerticalLayout implements
         if (personList == null) {
             personList = new PersonList(this);
         } return personList;
-    }
-
-    // lazy initialization pattern
-    private CommandForm getCommandForm() {
-        if (commandForm == null) {
-            commandForm = new CommandForm(this);
-        } return commandForm;
     }
 
     // lazy initialization pattern
@@ -185,36 +165,18 @@ public class PersonWidget extends VerticalLayout implements
         return helpWindow;
     }
 
-    SharingOptions sharingOptions = null;
-
-    private SharingOptions getSharingOptions() {
-        if (sharingOptions == null) {
-            sharingOptions = new SharingOptions();
-        }
-        return sharingOptions;
-    }
-
-    private SearchView searchView = null;
-
-    private SearchView getSearchView() {
-        if (searchView == null) {
-            searchView = new SearchView(this);
-        }
-        return searchView;
-    }
-
     private HorizontalLayout createToolbar() {
         HorizontalLayout lo = new HorizontalLayout();
 
-        newPerson.setIcon(new ThemeResource("icons/32/document-add.png"));
+        newPerson.setIcon(new ThemeResource("icons/32/users.png"));
         newPerson.addListener((Button.ClickListener) this);
         lo.addComponent(newPerson);
-        search.setIcon(new ThemeResource("icons/32/folder-add.png"));
+        newUser.setIcon(new ThemeResource("icons/32/user.png"));
+        newUser.addListener((Button.ClickListener) this);
+        lo.addComponent(newUser);
+        search.setIcon(new ThemeResource("icons/32/search.png"));
         search.addListener((Button.ClickListener) this);
         lo.addComponent(search);
-        share.setIcon(new ThemeResource("icons/32/users.png"));
-        share.addListener((Button.ClickListener) this);
-        lo.addComponent(share);
         help.setIcon(new ThemeResource("icons/32/help.png"));
         help.addListener((Button.ClickListener) this);
         lo.addComponent(help);
@@ -229,10 +191,6 @@ public class PersonWidget extends VerticalLayout implements
         lo.setExpandRatio(em, 1);
         lo.setStyleName("toolbar");
         return lo;
-    }
-
-    private void showSearchView() {
-        horizontalSplit.setSecondComponent(getSearchView());
     }
 
     @Override
@@ -251,29 +209,18 @@ public class PersonWidget extends VerticalLayout implements
     public void buttonClick(ClickEvent event) {
         final Button source = event.getButton();
         if (source == search) {
-            showSearchView();
+            setLeftForm(PersonWidget.FormType.FIND);
         } else if (source == newPerson) {
-            addNewContanct();
+            setLeftForm(PersonWidget.FormType.PERSON);
         } else if (source == help) {
             app.getMainWindow().addWindow(getHelpWindow());
-        } else if (source == share) {
-            app.getMainWindow().addWindow(getSharingOptions());
-        } else if (source == main) {
-            setLeftForm(FormType.COMMANDS);
+        } else if (source == newUser) {
+            setLeftForm(PersonWidget.FormType.USER);
         } else if (source == discard) {
             leftForm.getForm().discard();
         } else if (source == save) {
             leftForm.getForm().save();
         }
-    }
-
-    private void showListView() {
-        //setMainComponent(getListView());
-    }
-
-    private void addNewContanct() {
-        showListView();
-        //personForm.addContact();
     }
 
     @Override
